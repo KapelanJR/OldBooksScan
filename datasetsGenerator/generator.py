fd2b3699dc70c91de6366febc0a0b3f46ce3487f
Marcin Tajsner <marcin.tajsner@gmail.com>
2020-05-30 15:12:17 +0200

    datasetGenerator v. 0.3

--- datasetsGenerator/generator.py
+++ datasetsGenerator/generator.py
@@ -1,27 +1,38 @@
 import os
 import fitz
 
+#Ustawienia
 booksDirName = "books"
 datasetDirName = "datasets"
 datasetOutputName = "gen1"
+defaultCharLimit = 200
+pageOffset = 5
 
-#charList = [
-#    'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż',
-#    'A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'R', 'S', 'Ś', 'T', 'U', 'W', 'Y', 'Z', 'Ź', 'Ż'
-#    ]
+#Klasa posiadająca dane o danym znaku
+class charData:
+    def __init__(self, char, charLimit = defaultCharLimit):
+        self.char = char
+        self.charLimit = charLimit
+        self.count = 0
 
+#Lista znaków
 charList = [
-    'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż',
+    charData('a'), charData('ą'), charData('b'), charData('c'), charData('ć'), charData('d'), charData('e'), charData('ę'), charData('f'), charData('g'), 
+    charData('h'), charData('i'), charData('j'), charData('k'), charData('l'), charData('ł'), charData('m'), charData('n'), charData('ń'), charData('o'), 
+    charData('ó'), charData('p'), charData('r'), charData('s'), charData('ś'), charData('t'), charData('u'), charData('w'), charData('y'), charData('z'), 
+    charData('ź'), charData('ż')
     ]
 
-
+#Definicja ścieżki bezwzględnej
 dirPath = os.path.dirname(__file__) + "/"
 
+#Definicja zmiennych globalnych
 amountOfBooks = 0
 currentBookNumber = 0
 currentBookPageCount = 0
 currentPageNumber = 0
 
+#Pobierz ścieżki książek (pdf) i zapisz je w liście "booksPaths"
 def getBooksPaths():
     booksDirPath = dirPath + booksDirName
     booksPaths = []
@@ -34,6 +45,7 @@ def getBooksPaths():
 
     return booksPaths
 
+#Utwórz folder dla każdej książki i iteruj przez jej strony
 def createBookDataset(bookPath):
     global currentBookPageCount
     global currentPageNumber
@@ -44,31 +56,35 @@ def createBookDataset(bookPath):
         os.makedirs(dirPath + datasetDirName + "/" + datasetOutputName + "/book" + str(currentBookNumber))
 
     currentBookPageCount = book.pageCount
-    for page in book:
+    currentPageNumber = 0
+
+    for i in range(pageOffset + 1, currentBookPageCount - pageOffset):
         currentPageNumber += 1
-
-        if not os.path.exists(dirPath + datasetDirName + "/" + datasetOutputName + "/book" + str(currentBookNumber) + "/p" + str(currentPageNumber)):
-            os.makedirs(dirPath + datasetDirName + "/" + datasetOutputName + "/book" + str(currentBookNumber) + "/p" + str(currentPageNumber))
-
         generationLoadingBar()
-        createPageDataset(page)
-    
+        createPageDataset(book[i])
+
+#Utwórz folder dla każdego znaku i wyszukaj ich występowania na danej stronie
 def createPageDataset(page):
-    for char in charList:
-        if not os.path.exists(dirPath + datasetDirName + "/" + datasetOutputName + "/book" + str(currentBookNumber) + "/p" + str(currentPageNumber) + "/" + char):
-            os.makedirs(dirPath + datasetDirName + "/" + datasetOutputName + "/book" + str(currentBookNumber) + "/p" + str(currentPageNumber) + "/" + char)
+    for charData in charList:
+        if not os.path.exists(dirPath + datasetDirName + "/" + datasetOutputName + "/book" + str(currentBookNumber) + "/" + charData.char):
+            os.makedirs(dirPath + datasetDirName + "/" + datasetOutputName + "/book" + str(currentBookNumber) + "/" + charData.char)
         
-        generateCharDataset(char, page.searchFor(char, 1000), page)
+        generateCharDataset(charData, page.searchFor(charData.char, 1000), page)
 
-def generateCharDataset(char, areas, page):
-    i = 0
+#Zapisz znalezione fragmenty renderu PDF z danym szukanym znakiem jako obrazki PNG, każdy następny obrazek (danego znaku) ma nazwę o 1 większą niż poprzedni
+def generateCharDataset(charData, areas, page):
     for area in areas:
-        i += 1
-        iarea = area.irect
-        mat = fitz.Matrix(4, 4)
-        img = page.getPixmap(matrix = mat, clip = iarea)
-        img.writeImage(dirPath + datasetDirName + "/" + datasetOutputName + "/book" + str(currentBookNumber) + "/p" + str(currentPageNumber) + "/" + char + "/" + str(i) + ".png")
+        if charData.count < charData.charLimit:
+            charData.count += 1
+            img = page.getPixmap(matrix = fitz.Matrix(4, 4), clip = area.irect)
+            img.writeImage(dirPath + datasetDirName + "/" + datasetOutputName + "/book" + str(currentBookNumber) + "/" + charData.char + "/" + str(charData.count) + ".png")
 
+#Po wygenerowaniu zbioru danych dla danej książki, wyczyść licznik występowania danego znaku
+def clearCharsCount():
+    for charData in charList:
+        charData.count = 0
+
+#Rysuj ładny pasek postępu
 def generationLoadingBar():
     global amountOfBooks
     global currentBookNumber
@@ -77,7 +93,7 @@ def generationLoadingBar():
 
     barLength = 50
 
-    percentDone = (currentPageNumber / currentBookPageCount) * 100
+    percentDone = (currentPageNumber / (currentBookPageCount - pageOffset - pageOffset - 1)) * 100
     barDone = int((barLength / 100) * percentDone)
 
     print("Book", currentBookNumber, "from", amountOfBooks, "generation progress: ", end="")
@@ -104,9 +120,11 @@ def main():
         if not os.path.exists(dirPath + datasetDirName + "/" + datasetOutputName):
             os.makedirs(dirPath + datasetDirName + "/" + datasetOutputName)
 
+        #Iteruj po ksiązkach
         for bookPath in booksPaths:
             currentBookNumber += 1
             createBookDataset(bookPath)
+            clearCharsCount()
 
         print("Finished")
     else:
