d997421a1041cc8cd3f609955a54cdeedb49bdd6
Kapi <kacperszmitk12@gmail.com>
2020-06-02 18:19:55 +0200

    aaccept
    Signed-off-by: Kapi <kacperszmitk12@gmail.com>

--- datasetsGenerator/test.py
+++ datasetsGenerator/test.py
@@ -1,26 +1,13 @@
-import os
-import fitz
-
-def searchForCaseSensitive(textBlocks, charToSearch):
-    areas = []
-    for textBlock in textBlocks:
-        lines = textBlock["lines"]
-        for line in lines:
-            spans = line["spans"]
-            for span in spans:
-                chars = span["chars"]
-                for char in chars:
-                    if char["c"] == charToSearch:
-                        areas.append(fitz.Rect(char["bbox"][:4]))
-    
-    return areas
+from keras import layers
+from keras import models
+from keras.preprocessing import image
+import os,shutil
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
 
 
-bookPath = os.path.dirname(__file__) + "/books/book1.pdf"
-doc = fitz.open(bookPath)
-page = doc[12]
-text = page.getText("rawdict", flags=fitz.TEXT_PRESERVE_LIGATURES + fitz.TEXT_PRESERVE_WHITESPACE)
-textBlocks = text["blocks"]
-char = 'a'
-areas = searchForCaseSensitive(textBlocks, char)
-print("TEST")
+base_dir = 'D:\\Progamowanie Python\\Uczenie Maszynowe\\datasetsGenerator\\datasets\\polish_lower_and_upper1'
+
+
+
