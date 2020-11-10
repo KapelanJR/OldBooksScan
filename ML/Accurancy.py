84694c825ee910537fb9bdf2b20ee3d70723b5a7
KapelanJR <kacperszmitk@wp.pl>
2020-11-10 23:45:28 +0100

    Not gut

--- ML/Accurancy.py
+++ ML/Accurancy.py
@@ -1,34 +1,38 @@
-from sklearn.metrics import classification_report,confusion_matrix
-from keras.models import load_model
-from keras import models
-from keras.preprocessing import image
+import warnings  
+with warnings.catch_warnings():  
+    warnings.filterwarnings("ignore",category=FutureWarning)
+    from keras_preprocessing.image import ImageDataGenerator
+    import numpy as np    
+    from keras.models import load_model
+    from keras import models
+    from sklearn.metrics import classification_report,confusion_matrix
 import os,shutil
-import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
+import pandas as pd
 from SingleUseFunctions import *
 from os import listdir
 from os.path import isfile, join
 
+
 #Directory with stored files
-dir = "./"
+dir = "./chars"
 
 files = [f for f in listdir(dir) if isfile(join(dir, f))]
 
-create_CSV(files,"")
+create_CSV(files,"./TrainLabels.csv")
 
 traindf = pd.read_csv("./TrainLabels.csv",dtype=str)
 traindf["id"]=traindf["id"].apply(append_ext)
-datagen=ImageDataGenerator(rescale=1./255)
+datagen=ImageDataGenerator(rescale=1./255,validation_split=0)
 
-model = models.load_model("./test.h5")
+model = models.load_model("./ML/test.h5")
 
 test_generator=datagen.flow_from_dataframe(
     dataframe=traindf,
-    directory= "./datasetsGenerator/datasets/test",
+    directory= dir,
     x_col="id",
     y_col="label",
-    batch_size=90,
-    shuffle=False,
+    batch_size=44,
+    shuffle=True,
     class_mode="categorical",
     target_size=(20,32)
 )
