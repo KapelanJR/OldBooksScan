5cc628eb6257f5824a0719024c022ecacdf52683
Marcin Tajsner <marcin.tajsner@gmail.com>
2020-11-12 17:37:40 +0100

    Accurancy v 1.4

--- ML/Accurancy.py
+++ ML/Accurancy.py
@@ -9,27 +9,32 @@ with warnings.catch_warnings():
 import os,shutil
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
+from SingleUseFunctions import *
 
-
-test_dir = './polish_1_hd/test'
-
+test_dir = "./readyDatasets/polish_1_hd/test"
 
 test_datagen = ImageDataGenerator(
     rescale=1./255
 )
 
 test_generator = test_datagen.flow_from_directory(
-    test_dir,target_size=(20,32),batch_size=2,class_mode='categorical',shuffle=False)
+    test_dir,target_size=(20,32),batch_size=71,class_mode='categorical'
+)
+
+model = models.load_model("./test_new.h5")
+pred = model.predict_generator(test_generator,steps=test_generator.n//test_generator.batch_size)
+
+#tablica z przewiywanymi wartosciami
+li = []
 
 model = models.load_model("./test.h5")
 #scores = model.evaluate_generator(test_generator,10000)
 pred = model.predict_generator(test_generator,steps=2)
 
 
-# Zamiana kluczy z wartościani wynik {indeks:unicode litery}
-inv_map = {v: k for k, v in test_generator.class_indices.items()}
-with open('./labels.txt', 'w') as file:
-    json.dump(inv_map, file)
-
-for i in pred:
-    print(inv_map[np.argmax(i)])
+#tablica z wartosciami jakie maja byc
+vi = []
+for x in test_generator.classes:
+    vi.append(fi[x])
+        
+print(classification_report(vi, li))
