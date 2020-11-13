31c1c22fd795d879d04d71c9828a708c005bfce6
KapelanJR <kacperszmitk@wp.pl>
2020-11-13 02:38:47 +0100

    Train v 1.4.2

--- ML/Train.py
+++ ML/Train.py
@@ -15,43 +15,44 @@ def main():
     train_dir = "./readyDatasets/polish_1_hd/train"
     validation_dir = "./readyDatasets/polish_1_hd/validation"
     
-    #Generatory zwracające wszystkie pliki z danego foldera i jego podfolderów
-    #Podfolder jest traktowany jako odzielna etykeita
-    #Przeskalowane z 3 wymiarów (RGB) do 1 wymiaru 
-    train_datagen = ImageDataGenerator(
-        rescale=1./255
-    )
-    validation_datagen = ImageDataGenerator(rescale=1./255)
 
-    train_generator = train_datagen.flow_from_directory(
-        train_dir,target_size=(20,32),batch_size=90,class_mode='categorical'
-    )
+train_dir = "polish_1_hd/train"
+validation_dir = "polish_1_hd/validation"
+#Generatory zwracające wszystkie pliki z danego foldera i jego podfolderów
+#Podfolder jest traktowany jako odzielna etykeita
+#Przeskalowane z 3 wymiarów (RGB) do 1 wymiaru 
+train_datagen = ImageDataGenerator(
+    rescale=1./255
+)
+validation_datagen = ImageDataGenerator(rescale=1./255)
 
-    validation_generator = validation_datagen.flow_from_directory(
-        validation_dir,target_size=(20,32),batch_size=90,class_mode='categorical'
-    )
+train_generator = train_datagen.flow_from_directory(
+    train_dir,target_size=(20,32),batch_size=90,class_mode='categorical',shuffle=True
+)
 
-    #Architektura sieci
-    model = models.Sequential()
-    model.add(layers.Conv2D(256,(3,3),activation='relu',input_shape=(20,32,3)))
-    model.add(layers.MaxPool2D((2,2)))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(256,activation='relu'))
-    model.add(layers.Dense(1024,activation='relu'))
-    model.add(layers.Dense(88,activation='softmax'))
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,target_size=(20,32),batch_size=90,class_mode='categorical',shuffle=True
+)
 
-    model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['acc'])
 
-    history = model.fit_generator(
-        train_generator,
-        steps_per_epoch=1900,
-        epochs=6,
-        validation_data=validation_generator,
-        validation_steps=504,
-    )
+#Architektura sieci
+model = models.Sequential()
+model.add(layers.Conv2D(256,(3,3),activation='relu',input_shape=(20,32,3)))
+model.add(layers.MaxPool2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(256,activation='relu'))
+model.add(layers.Dense(1024,activation='relu'))
+model.add(layers.Dense(88,activation='softmax'))
 
-    model.save("test_new.h5")
+model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['acc'])
 
-if __name__ == "__main__":
-    main()
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=1900,
+    epochs=6,
+    validation_data=validation_generator,
+    validation_steps=504,
+)
+
+model.save("test.h5")
 
